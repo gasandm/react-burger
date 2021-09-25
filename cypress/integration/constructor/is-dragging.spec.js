7552c35251216bd9d60a59ebb9faa2624a88fb62
Гасанбек Джарбаев <gasand92@yandex.ru>
2021-09-25 10:35:20 +0300

    test fix

--- cypress/integration/constructor/is-dragging.spec.js
+++ cypress/integration/constructor/is-dragging.spec.js
@@ -4,6 +4,7 @@ describe('service is available', function() {
     });
 
     it('should correct drag and drop element', function() {
+        cy.wait(1000);
         cy.get('.burger-constructor_ingredientsListInner__2u2hQ>div').should('not.exist');
         cy.get('.ingredient_ingredient__3tAi7').eq(2).trigger('dragstart'); 
         cy.get('.burger-constructor_ingredientsList__3vMA4').trigger('drop');
